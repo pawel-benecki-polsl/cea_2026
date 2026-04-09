9fd0caa038cd38023782dad2540a60dd3bac1126
pawel-benecki-polsl <84968031+pawel-benecki-polsl@users.noreply.github.com>
2026-04-09 09:37:13 +0200

    Controller

--- src/main/java/pl/polsl/inf/cea/reservation/controller/ReservationController.java
+++ src/main/java/pl/polsl/inf/cea/reservation/controller/ReservationController.java
@@ -1,8 +1,12 @@
 package pl.polsl.inf.cea.reservation.controller;
 
 import org.springframework.web.bind.annotation.*;
+import pl.polsl.inf.cea.reservation.ReservationApplication;
 import pl.polsl.inf.cea.reservation.domain.Reservation;
+import pl.polsl.inf.cea.reservation.domain.Seat;
 import pl.polsl.inf.cea.reservation.service.ReservationService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reservations")
@@ -18,5 +22,16 @@ public class ReservationController {
     public Reservation reserve(@RequestParam Long seatId,
                                @RequestParam String userId) {
         return reservationService.reserve(seatId, userId);
+    }
+
+
+    @GetMapping
+    public List<Reservation> retrieveAll() {
+        return reservationService.retrieveAllReservations();
+    }
+
+    @GetMapping("/freeSeats")
+    public List<Seat> getFreeSeatsForEvent(Long eventId) {
+        return reservationService.getFreeSeatsForEvent(eventId);
     }
 }
