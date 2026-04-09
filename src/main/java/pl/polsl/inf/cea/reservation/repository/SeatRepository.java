25bd1414948947852d1010eda600989366a93236
pawel-benecki-polsl <84968031+pawel-benecki-polsl@users.noreply.github.com>
2026-04-09 09:38:26 +0200

    l3

--- src/main/java/pl/polsl/inf/cea/reservation/repository/SeatRepository.java
+++ src/main/java/pl/polsl/inf/cea/reservation/repository/SeatRepository.java
@@ -2,6 +2,11 @@ package pl.polsl.inf.cea.reservation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.polsl.inf.cea.reservation.domain.Seat;
+import pl.polsl.inf.cea.reservation.domain.SeatStatus;
+
+import java.util.List;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    List<Seat> findByEventIdAndStatus(Long eventIt, SeatStatus status);
 }
